6d4a4e79202d251a7b8e061545126ca9ad7734f1
Felipe Anchieta Santos Costa <felipeanchieta@hotmail.com.br>
2017-06-02 01:29:45 -0300

    #2 Fixed two out of three unit tests

--- test/nanook/balance_test.clj
+++ test/nanook/balance_test.clj
@@ -15,24 +15,30 @@
 (defn- put-credit
   "Helper function to put some credit in an account throught a handler"
   [acc-number handler]
-  (handler (-> (mock/request
-                :post
-                (format "/accounts/%s/credit" acc-number)
-                (json/generate-string {:amount      1000.0
-                                       :description "Salary"})))))
-(defn- get-credit ; todo: do-debit
+  (let [response (handler (-> (mock/request
+                               :post
+                               (format "/accounts/%s/credit" acc-number)
+                               (json/generate-string {:amount      1000.0
+                                                      :description "Salary"}))
+                              (mock/content-type "application/json")))]
+    (is (= (:status response) 200))))
+
+(defn- do-debit
   "Helper function to get money from an account throught a handler"
   [acc-number handler]
-  (handler (-> (mock/request
-                :post
-                (format "/accounts/%s/debit" acc-number)
-                (json/generate-string {:amount      1000.0
-                                       :description "Salary"})))))
+  (let [response (handler (-> (mock/request
+                               :post
+                               (format "/accounts/%s/debit" acc-number)
+                               (json/generate-string {:amount      1000.0
+                                                      :description "Nubank Bill"}))
+                              (mock/content-type "application/json")))]
+    (is (= (:status response) 200))))
 
 (deftest balance-tests
   (testing
    "The balance of a client that has only incomes"
     (do
+      ;;; Puts 3000.0
       (dotimes [_ 3] (put-credit "12345" app))
       (let [response (app (mock/request
                            :get
@@ -42,25 +48,27 @@
         (is (= (float (:balance body)) 3000.0)))))
 
   (testing
-   "The balance of a client that has only debts like mine"
+   "The balance of a client that has only debts like me"
     (do
-      (dotimes [_ 3] (get-credit "12345" app))
+      ;;; Gets 3000.0 out of 0.0
+      (dotimes [_ 3] (do-debit "12345" app))
       (let [response (app (mock/request
                            :get
                            "/accounts/12345/balance"))
             body     (parse-body (:body response))]
         (is (= (:status response) 200))
-        (is (= (float (:balance body)) 3000.0)))))
+        (is (= (float (:balance body)) -3000.0)))))
 
   (testing
    "The balance of a client that is doing well, with some credits
        and some debits"
     (do
+      ;;; Puts 3000.0 and gets 2000.0
       (dotimes [_ 3] (put-credit "12345" app))
-      (dotimes [_ 2] (get-credit "12345" app))
+      (dotimes [_ 2] (do-debit "12345" app))
       (let [response (app (mock/request
                            :get
                            "/accounts/12345/balance"))
             body     (parse-body (:body response))]
         (is (= (:status response) 200))
-        (is (= (float (:balance body)) 3000.0))))))
+        (is (= (float (:balance body)) 1000.0))))))
